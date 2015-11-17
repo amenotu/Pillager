c994bd2f37beb07d59ec0fc7bea90060d845120c
amenotu <kyabut@gmail.com>
2015-11-16 20:38:39 -0800

    server.js now serves static files

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ app.get('/', function (req, res) {
 });
 
 app.use(express.static(__dirname + '/public'));
+<<<<<<< 8409fd96aad18efa57526d913ef34a135e829997
+=======
+
+var server = app.listen(app.get('port'), function () {
+  var host = server.address().address;
+  var port = server.address().port;
+>>>>>>> server.js now serves static files
 
 app.listen(app.get('port'));
 console.log('Pillager app now listening on port ' + app.get('port'));
