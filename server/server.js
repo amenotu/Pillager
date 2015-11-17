b9a804a4f643b39d144b621769a457a0ea0b340d
amenotu <kyabut@gmail.com>
2015-11-16 20:38:39 -0800

    server.js now serves static files

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.use(express.static(__dirname + '/public'));
+
 var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
